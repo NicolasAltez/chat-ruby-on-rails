9b5af31a34fcfb4203111e34ee685de7a48ad5e4
nicolasAltez <nicolas.altez@these.com.uy>
2023-05-29 15:42:22 -0300

    modified message_controller_test

--- spec/controllers/message_controller_spec.rb
+++ spec/controllers/message_controller_spec.rb
@@ -1,11 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe MessagesController, type: :controller do
-  include Devise::Test::ControllerHelpers
-
   describe "GET index" do
     it "renders the index template" do
-      get messages_path
+      get :index
       expect(response).to render_template(:index)
     end
   end
@@ -14,12 +12,12 @@ RSpec.describe MessagesController, type: :controller do
     context "with valid parameters" do
       it "creates a new message" do
         expect {
-          post messages_path, params: { message: { content: "Hola" } }
+          post :create, params: { message: { content: "Hola" } }
         }.to change(Message, :count).by(1)
       end
 
       it "redirects to the index page" do
-        post messages_path, params: { message: { content: "Hola" } }
+        post :create, params: { message: { content: "Hola" } }
         expect(response).to redirect_to(messages_path)
       end
     end
@@ -27,12 +25,12 @@ RSpec.describe MessagesController, type: :controller do
     context "with invalid parameters" do
       it "does not create a new message" do
         expect {
-          post messages_path, params: { message: { content: nil } }
+          post :create, params: { message: { content: nil } }
         }.not_to change(Message, :count)
       end
 
       it "renders the index template" do
-        post messages_path, params: { message: { content: nil } }
+        post :create, params: { message: { content: nil } }
         expect(response).to render_template(:index)
       end
     end
