a828d7d791d11dab3a025ffdd4c8993a4cd9384f
nicolasAltez <nicolas.altez@these.com.uy>
2023-05-29 15:25:44 -0300

    add messages_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,25 @@
 class MessagesController < ApplicationController
   def index
+    @messages = Message.all
+  end
+
+  def new
+    @message = Message.new
   end
 
   def create
+    @message = Message.new(message_params)
+
+    if @message.save
+      redirect_to messages_path
+    else
+      @messages = Message.all
+      render :index , status: :unprocessable_entity
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
   end
 end
