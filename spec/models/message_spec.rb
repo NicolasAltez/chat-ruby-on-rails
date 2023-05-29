e37a776d979b8a9b4ff7f48b989c9c886207d510
nicolasAltez <nicolas.altez@these.com.uy>
2023-05-29 15:04:05 -0300

    add test model message

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with content" do
+    message = Message.new(content: "Hello!")
+    expect(message).to be_valid
+  end
+
+  it "is invalid without content" do
+    message = Message.new(content: nil)
+    expect(message).to be_invalid
+  end
 end
